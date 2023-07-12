6dbdce2176d35de8c03c835ff13fb2ab55e32d7c
Eliran Amar <eliranamar7@gmail.com>
2023-07-12 10:37:32 +0300

    Add prettier

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -1,3 +1,12 @@
 export default function handler(req, res) {
-    res.status(200).json({ name: 'Denisssss!@@#!#!%@ :-D O_o' })
+    console.log('req.method', req.method)
+
+    try {
+        res.status(200).json({ name: 'Denisssss!@@#!#!%@ :-D O_o' })
+    } catch (e) {
+        res.status(500).json({
+            statusCode: 500,
+            message: e.message || 'Error!',
+        })
+    }
 }
