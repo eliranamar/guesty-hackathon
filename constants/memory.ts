3a262c56e0ed8d593f7de0de46fa54cb6bf6205c
Eliran Amar <eliranamar7@gmail.com>
2023-07-12 14:55:23 +0300

    add consts

---
+++ constants/memory.ts
@@ -0,0 +1,14 @@
+export enum MEMORY_SOURCE {
+    AI = 'AI',
+    HOST = 'HOST',
+}
+
+export enum MEMORY_TYPE {
+    TRIP = 'TRIP',
+    RESTAURANT = 'RESTAURANT',
+    CONCERT = 'CONCERT',
+    MOVIE = 'MOVIE',
+    SPORT = 'SPORT',
+    VOLUNTEERING = 'VOLUNTEERING',
+    OTHER = 'OTHER',
+}
